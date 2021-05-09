bbf38b089e606a134d6ea2159a4491e17b55a214
Usman Zaheerm <usmanzaheer1995@gmail.com>
2021-05-09 18:55:36 +0500

    fixed cron job time

--- src/utils/schedular.ts
+++ src/utils/schedular.ts
@@ -2,8 +2,9 @@ import cron from 'node-cron';
 
 import fetchIndegoData from './fetch-indigo-data';
 
+// if env variable is set to true, this job will run after every hour
 export default (process.env.ENABLE_CRON_JOB === 'true')
-  ? cron.schedule('* * * * * *', () => {
+  ? cron.schedule('0 0 */1 * * *', () => {
     console.log('running job to fetch and save data from indego API...');
     try {
       fetchIndegoData();
